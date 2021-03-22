c8650000f469d8285d92946411bf9a06945d0e00
Rekke-uk <mazcm2013@gmail.com>
2021-03-22 09:56:03 -0300

    Desafio 19 de Marzo c3

--- Desafio para la Clase del 23 de Marzo/js/app.js
+++ Desafio para la Clase del 23 de Marzo/js/app.js
@@ -1,38 +1,40 @@
-let carrito = JSON.parse(localStorage.getItem(`carrito`)) || [];
-
-// Agregar productos al carrito.
-function agregarProducto() {
-    let producto = prompt(`Ingrese un producto. Si quieres comprar otro producto hace click en Cancelar`)
-    if (producto) {
-        carrito.push(producto.trim().toUpperCase())
-
-        producto = prompt(`Ingrese un producto. Si quieres comprar otro producto hace click en Cancelar`)
-
-        localStorage.setItem(`productos`, JSON.stringify(carrito))
-        listarProductos()
+let products = JSON.parse(localStorage.getItem(`products`)) || [];
+class Product {
+    constructor(name, price) {
+        this.name = name,
+            this.price = price
     }
 }
 
-// Lista de los productos en el carrito.
-function listarProductos() {
-    console.log(`Su lista de productos es la siguiente`)
-    for (let i = 0; i < carrito.length; i++) {
-        console.log(`${i + 1} ${carrito[i]}`)
-    }
+function buyProduct(name, price) {
+    let product = new Product(name, price)
+    products.push(product)
+    localStorage.setItem('products', JSON.stringify(products))
 }
 
-// Eliminar productos del carrito.
-function eliminarProducto(producto = prompt(`Ingrese el producto a eliminar.`)) {
-    if (producto) {
-        let index = gorrito.indexOf(producto.trim().toUpperCase())
-        if (index != -1) {
-            let validar = confirm(`¿Estas seguro que quieres eliminar el producto ${producto.trim().toUpperCase()}?`)
-            if (validar) {
-                carrito.splice(index, 1);
-                console.log(`Se elimino ${producto.trim().toUpperCase()} del carrito.`)
-            }
-        } else {
-            console.warn(`El producto no existe en el carrito.`)
-        }
-    }
+function productList() {
+    let list = document.querySelector("#list")
+    list.innerText = " "
+    products.map((item) => {
+        let div = document.createElement("div")
+        div.classList.add("card-body")
+        div.innerText = `${item.name} $ ${item.price}`
+        list.appendChild(div, list)
+    })
 }
+
+// // Eliminar productos del carrito.
+// function eliminarProducto(producto = prompt(`Ingrese el producto a eliminar.`)) {
+//     if (producto) {
+//         let index = gorrito.indexOf(producto.trim().toUpperCase())
+//         if (index != -1) {
+//             let validar = confirm(`¿Estas seguro que quieres eliminar el producto ${producto.trim().toUpperCase()}?`)
+//             if (validar) {
+//                 carrito.splice(index, 1);
+//                 console.log(`Se elimino ${producto.trim().toUpperCase()} del carrito.`)
+//             }
+//         } else {
+//             console.warn(`El producto no existe en el carrito.`)
+//         }
+//     }
+// }
